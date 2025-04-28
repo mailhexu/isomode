6d9f3d1b4420fa90d7baaf3353cdb6c1641b8c1a
mailhexu <mailhexu@gmail.com>
2025-04-28 11:52:14 +0200

    update dependencies in setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 setup(
     name='isomode',
     version='0.1',
-    description='generate distorted structure from phonon/isodistort output',
+    description='Label/generate distorted structure from phonon/isodistort output',
     author='Xu He',
     author_email='mailhexu@gmail.com',
     license='MIT',
     packages=find_packages(),
     package_data={},
-    install_requires=['numpy', 'requests', 'bs4', 'ase', 'spglib', 'abipy'],
+    install_requires=['numpy==1.26.4','scipy=1.10.0', 'requests', 'bs4', 'ase', 'spglib', 'abipy==0.9.7', 'pymatgen==2025.4.20' ,'lxml'],   
     scripts=['scripts/view_distort.py', 'scripts/anamode.py'
         ],
     classifiers=[
